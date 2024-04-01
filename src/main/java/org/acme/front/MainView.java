74b887a4c498d6b6370acaecc4412f3b7ead13bb
Maxence Guenin <maxence@dataraxys.com>
2024-04-01 15:33:37 +0200

    creation de la grill

--- src/main/java/org/acme/front/MainView.java
+++ src/main/java/org/acme/front/MainView.java
@@ -4,6 +4,8 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.RouterLink;
 import jakarta.inject.Inject;
@@ -33,20 +35,21 @@ UtilisateurService utilisateurService;
         TextField prenom = new TextField("Prenom");
         DatePicker date_naissance = new DatePicker("Date de naissance");
         TextField adresse = new TextField("Adresse");
-        TextField telephone = new TextField("Telephone");
-        TextField email = new TextField("Email");
-        TextField code_postal = new TextField("Code postal");
+        NumberField telephone = new NumberField("Telephone");
+        EmailField email = new EmailField("Email");
+        NumberField code_postal = new NumberField("Code postal");
         layout.add(nom, prenom, date_naissance, adresse, telephone, email, code_postal);
         add(layout);
+
         Button button = new Button("Click me",
                 e -> {
                     utilisateur.setNom(nom.getValue());
                     utilisateur.setPrenom(prenom.getValue());
                     utilisateur.setDate_naissance(date_naissance.getValue().atStartOfDay());
                     utilisateur.setAdresse(adresse.getValue());
-                    utilisateur.setTelephone(telephone.getValue());
+                    utilisateur.setTelephone(telephone.getValue().toString());
                     utilisateur.setEmail(email.getValue());
-                    utilisateur.setCode_postal(code_postal.getValue());
+                    utilisateur.setCode_postal(code_postal.getValue().toString());
                     utilisateurService.addUtilisateur(utilisateur);
                 });
         add(button);
