c636bde2a32c84709bfaf64b0077a1aa23dbe84f
Shirin <90757378+juandelsantos@users.noreply.github.com>
2024-04-05 18:31:35 +0200

    des trucs

---
+++ src/main/java/org/acme/front/CursusListView.java
@@ -0,0 +1,47 @@
+package org.acme.front;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import jakarta.annotation.PostConstruct;
+import jakarta.inject.Inject;
+import org.acme.model.Situation_Scolaire;
+import org.acme.service.CursusService;
+
+@Route("cursus-list")
+public class CursusListView extends VerticalLayout {
+    @Inject
+    private CursusService cursusService;
+
+    private Grid<Situation_Scolaire> grid = new Grid<>(Situation_Scolaire.class);
+
+    public CursusListView() {
+        add(grid);
+    }
+
+    @PostConstruct
+    private void init() {
+        grid.setColumns("dernier_diplome", "etablissement", "experience_professionnelle", "poste_occupe", "duree");
+        grid.setItems(cursusService.listAll());
+
+        grid.addComponentColumn(cursus -> new Button("Détails", click -> showCursusDetailsDialog(cursus)))
+                .setHeader("Actions");
+    }
+    private void showCursusDetailsDialog(Situation_Scolaire cursus) {
+        Dialog dialog = new Dialog();
+        dialog.add(new Div(new Text("Dernier diplôme: " + cursus.getDernier_diplome())));
+        dialog.add(new Div(new Text("Établissement: " + cursus.getEtablissement())));
+        dialog.add(new Div(new Text("Expérience professionnelle: " + cursus.getExperience_professionnelle())));
+        dialog.add(new Div(new Text("Poste occupé: " + cursus.getPoste_occupe())));
+        dialog.add(new Div(new Text("Durée: " + cursus.getDuree())));
+
+        Button closeButton = new Button("Fermer", e -> dialog.close());
+        dialog.add(closeButton);
+
+        dialog.open();
+    }
+}
